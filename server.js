0df6a4cf30ba696680a07303ae21df988f9501eb
Yonas Alem <yonalem21@gmail.com>
2021-11-02 17:52:30 +0300

    local control and model imported

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const User = require("./models/User");
+
+const port = 8000;
+const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`);
+});
 
 // CREATE
-app.post('/users',(req,res)=>{
+app.post("/users", (req, res) => {
   // User.create()
-})
+  const { name, email, password } = req.body;
 
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+
+  u;
+});
+
+app
+  .route("/users/:id")
+  // READ
+  .get((req, res) => {
+    // User.findById()
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  });
